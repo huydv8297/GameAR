8a8978603fbcf050a3de0e9b8080bc0cb1022b42
huydv8297 <huydv8297@gmail.com>
2019-01-03 08:29:00 +0700

    first commit

---
+++ GameRaceAR/Assets/GameCace/Scripts/Test/CarTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTest : MonoBehaviour {
+
+    public float MotorForce;
+    public WheelCollider FR;
+    public WheelCollider FL;
+    public WheelCollider RR;
+    public WheelCollider RL;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Vertical") * MotorForce;
+
+        RL.motorTorque = v;
+        RR.motorTorque = v;
+	}
+}
